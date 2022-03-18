cfe6e7a6a6f41200604d990ce1a8c8ac19f6c964
in-ch <sxin2949@naver.com>
2022-03-18 11:16:20 +0900

    슬라이드 적용

---
+++ src/pages/Slide/Slide1/Slide1.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./Slide1Style";
+
+
+const Slide1 = () => {
+    return (
+        <Container>
+        </Container>
+    );
+};
+
+export default Slide1;
--- src/pages/Slide/Slide1/Slide1Style.tsx
+++ src/pages/Slide/Slide1/Slide1Style.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width:100%;
-    height:100vh;
-    border:1px solid #000;
+    height:100%;
+    background-color:red;
 `;
